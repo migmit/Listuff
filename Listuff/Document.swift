157da80943019cc3378ccab7fc937ba6da4d8e53
migmit <migmit@gmail.com>
2021-01-02 14:33:30 +0100

    Added document structure

---
+++ Listuff/Document.swift
@@ -0,0 +1,124 @@
+//
+//  Document.swift
+//  Listuff
+//
+//  Created by MigMit on 02.01.2021.
+//
+
+import Foundation
+
+class Document {
+    class List {
+        var items: WAVLTree<Item>
+        var parent: ListParent?
+        init(items: WAVLTree<Item>, parent: ListParentContainer? = nil) {
+            self.items = items
+            self.parent = parent.map{ListParent(container: $0)}
+        }
+    }
+    enum Item {
+        case regular(value: RegularItem)
+        case numbered(value: NumberedList)
+        case sublist(value: Sublist)
+        var parent: List {
+            get {
+                switch(self) {
+                case .regular(value: let value): return value.parent
+                case .numbered(value: let value): return value.parent
+                case .sublist(value: let value): return value.parent
+                }
+            }
+            set(parent) {
+                switch(self) {
+                case .regular(value: let value): value.parent = parent
+                case .numbered(value: let value): value.parent = parent
+                case .sublist(value: let value): value.parent = parent
+                }
+            }
+        }
+        var this: WAVLTree<Item>.Node? {
+            get {
+                switch(self) {
+                case .regular(value: let value): return value.this
+                case .numbered(value: let value): return value.this
+                case .sublist(value: let value): return value.this
+                }
+            }
+            set(this) {
+                switch(self) {
+                case .regular(value: let value): value.this = this
+                case .numbered(value: let value): value.this = this
+                case .sublist(value: let value): value.this = this
+                }
+            }
+        }
+    }
+    class RegularItem {
+        var content: Line
+        var style: LineStyle
+        var parent: List
+        weak var this: WAVLTree<Item>.Node? = nil
+        init(content: Document.Line, style: Document.LineStyle, parent: Document.List) {
+            self.content = content
+            self.style = style
+            self.parent = parent
+        }
+    }
+    class NumberedList {
+        var items: WAVLTree<NumberedItem>
+        var parent: List
+        weak var this: WAVLTree<Item>.Node? = nil
+        internal init(items: WAVLTree<Document.NumberedItem>, parent: Document.List) {
+            self.items = items
+            self.parent = parent
+        }
+    }
+    class NumberedItem {
+        var content: Line
+        var sublist: List
+        var parent: NumberedList
+        weak var this: WAVLTree<NumberedItem>.Node? = nil
+        init(content: Document.Line, sublist: Document.List, parent: Document.NumberedList) {
+            self.content = content
+            self.sublist = sublist
+            self.parent = parent
+        }
+    }
+    class Sublist {
+        var list: List
+        var parent: List
+        weak var this: WAVLTree<Item>.Node? = nil
+        init(list: Document.List, parent: Document.List) {
+            self.list = list
+            self.parent = parent
+        }
+    }
+    class Line {
+        weak var content: WAVLTree<Line>.Node? = nil
+        var checked: Checked?
+        var parent: LineParent
+        init(checked: Document.Checked? = nil, parent: Document.LineParent) { // TOFIX
+            self.checked = checked
+            self.parent = parent
+        }
+    }
+    struct Checked {
+        var value: Bool
+    }
+    enum LineStyle {
+        case dash
+        case bullet
+    }
+    struct ListParent {
+        var container: ListParentContainer
+        weak var this: WAVLTree<List>.Node? = nil
+    }
+    enum ListParentContainer {
+        case sublist(value: Sublist)
+        case numbered(value: NumberedItem)
+    }
+    enum LineParent {
+        case regular(value: RegularItem)
+        case numbered(value: NumberedItem)
+    }
+}
